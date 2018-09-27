50c3b1144550e555299e9857758f881c3936af05
youngestdj <jessam7007@gmail.com>
2018-09-27 12:45:56 +0100

    update server.js to include default route

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-require('./api/routes/signup_routes.js')(app);
+require('./api/routes/routes.js')(app);
 require('./api/routes/order_routes.js')(app);
+require('./api/routes/signup_routes.js')(app);
 require('./api/routes/login_routes.js')(app);
 
 
