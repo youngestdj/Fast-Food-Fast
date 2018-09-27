dce946a80c63487403baea0450cde2cb67a3bb2d
youngestdj <jessam7007@gmail.com>
2018-09-27 18:06:28 +0100

    Add api endpoint for user to get menu options

--- api/routes/menu_routes.js
+++ api/routes/menu_routes.js
@@ -2,13 +2,15 @@ const database = require('../models/database.js');
 
 const db = new database.Database();
 
-const loginRouter = (app) => {
+const menuRouter = (app) => {
+  // API endpoint for admin to add new menu option
   app.post('/api/v1/menu/', (request, response) => {
     if (request.body.food && parseInt(request.body.price, 10) && request.body.quantifier) {
       const food = request.body.food.trim();
       const price = request.body.price.trim();
       const quantifier = request.body.quantifier.trim();
-      const query = `select food from menu where food='${request.body.food}'`;
+      // check if food is already in database
+      const query = `SELECT food FROM menu WHERE food='${request.body.food}'`;
       db.client.query(query)
         .then((res) => {
           if (!res.rows[0]) {
@@ -25,6 +27,19 @@ const loginRouter = (app) => {
       response.send({ status: 'error', message: 'invalid data' });
     }
   });
+
+  // API endpoint for user to get options
+  app.get('/api/v1/menu/', (request, response) => {
+    const query = 'SELECT id, food, price, quantifier FROM menu';
+    db.client.query(query)
+      .then((res) => {
+        if (res.rows) {
+          response.send(res.rows);
+        } else {
+          response.send({ status: 'success', message: 'No menu yet' });
+        }
+      });
+  });
 };
 
-module.exports = loginRouter;
+module.exports = menuRouter;
