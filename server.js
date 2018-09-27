3c959a22425fafec4b737019e37053a547b1048c
youngestdj <jessam7007@gmail.com>
2018-09-27 09:19:08 +0100

    update package.json to reflect new dependencies

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+require('./api/routes/routes.js')(app);
+require('./api/routes/signup_routes.js')(app);
+require('./api/routes/order_routes.js')(app);
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const routes = require('./api/routes/routes.js')(app);
-const signupRoutes = require('./api/routes/signup_routes.js')(app);
 
 const server = app.listen(process.env.PORT || 3001, () => {
   console.log('Listening on port %s...', server.address().port);
