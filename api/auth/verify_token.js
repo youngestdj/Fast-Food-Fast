d958778b20d3724d5a4f5060e16260389eadfe58
youngestdj <jessam7007@gmail.com>
2018-11-01 04:10:19 +0100

    Added error status to token auth middleware

--- api/auth/verify_token.js
+++ api/auth/verify_token.js
@@ -3,11 +3,11 @@ const jwt = require('jsonwebtoken');
 exports.verifyToken = (request, response, next) => {
   const token = request.headers['x-access-token'] || request.body.token || request.query.token;
   if (!token) {
-    return response.status(401).json({ auth: false, message: 'No token provided.' });
+    return response.status(401).json({ status: 'error', auth: false, message: 'No token provided.' });
   }
   jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) {
-      return response.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+      return response.status(500).json({ status: 'error', auth: false, message: 'Failed to authenticate token.' });
     }
     request.userId = decoded.id;
     request.role = decoded.role;
@@ -18,7 +18,7 @@ exports.verifyToken = (request, response, next) => {
 exports.verifyAdmin = (request, response, next) => {
   const { role } = request;
   if (role !== 'admin') {
-    return response.status(500).json({ auth: false, message: 'Only admin can access this resource' });
+    return response.status(500).json({ status: 'error', auth: false, message: 'Only admin can access this resource' });
   }
   next();
 };
