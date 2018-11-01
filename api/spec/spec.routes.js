c52c87ac3dc1e9d835161d3c1cbc6fa76abe9543
youngestdj <jessam7007@gmail.com>
2018-11-01 02:52:37 +0100

    write tests for user routes

--- api/spec/spec.routes.js
+++ api/spec/spec.routes.js
@@ -335,13 +335,13 @@ describe('Server', () => {
           expect(res.statusCode).toBe(200);
         });
     });
-    it('Should return 200 jessam2@joyson.com as email', async () => {
+    it('Should return jessam2@joyson.com as email', async () => {
       await request(app1)
         .get('/api/v1/user/2')
         .set('content-type', 'application/json')
         .set('x-access-token', token)
         .expect((res) => {
-          expect(res.body.message.email).toBe(200);
+          expect(res.body.message.email).toBe('jessam2@joyson.com');
         });
     });
 
